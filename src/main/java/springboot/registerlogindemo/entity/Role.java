cf544946e498cc68e5fc64328f06e0b255aa686c
billmazio <bmaziotis@windowslive.com>
2023-03-19 18:08:57 +0200

    Role.java

---
+++ src/main/java/springboot/registerlogindemo/entity/Role.java
@@ -0,0 +1,29 @@
+package springboot.registerlogindemo.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "roles")
+public class Role {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false,unique = true)
+    private String name;
+
+    @ManyToMany(mappedBy = "roles")
+    private List<User> users;
+
+}
